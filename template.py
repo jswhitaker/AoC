50aa7bfec12b5bf64dd442260e822e3190b42acc
Jake <jake.whitaker@gmail.com>
2020-12-06 22:24:17 -0800

    Created a template for starting from. Really basic

---
+++ template.py
@@ -0,0 +1,11 @@
+def main():
+    with open('day4-input.txt') as input_file:
+#        data = input_file.read()
+#        groups = data.split('\n\n')
+#     for line in input_file:
+    pass
+
+
+if __name__ == '__main__':
+    result = main()
+    print(result)
