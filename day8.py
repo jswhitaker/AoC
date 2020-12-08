3dd90384bc7875286518ae4027fc5e4de9d00d23
Jake <jake.whitaker@gmail.com>
2020-12-07 22:16:29 -0800

    Part 2  done. Might refactor this

--- day8.py
+++ day8.py
@@ -11,12 +11,7 @@ def process_instruction(operation, accumulator, instructions):
     return next_op, acc
 
 
-def main():
-    instructions = []
-    with open('day8-input.txt') as input_file:
-        for line in input_file:
-            line_split = line.rstrip().split()
-            instructions.append((line_split[0], int(line_split[1])))
+def run_program(instructions):
     hit_loop = False
     op = 0
     acc = 0
@@ -27,7 +22,32 @@ def main():
         op = next_op
         if next_op in visited:
             hit_loop = True
-    return acc
+        if next_op == len(instructions):
+            break
+    return acc, hit_loop
+
+
+def fix_program(instructions):
+    for i, instruction in enumerate(instructions):
+        new_instructions = instructions.copy()
+        if instruction[0] == 'jmp':
+            new_instructions[i] = ('nop', instruction[1])
+        elif instruction[0] == 'nop':
+            new_instructions[i] = ('jmp', instruction[1])
+        else:
+            continue
+        acc, hit_loop = run_program(new_instructions)
+        if not hit_loop:
+            return acc
+
+
+def main():
+    instructions = []
+    with open('day8-input.txt') as input_file:
+        for line in input_file:
+            line_split = line.rstrip().split()
+            instructions.append((line_split[0], int(line_split[1])))
+    return fix_program(instructions)
 
 
 if __name__ == '__main__':
