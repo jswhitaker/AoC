6d5fb670c6542a650567b193acd8239b1b252de0
Jake <jake.whitaker@gmail.com>
2020-12-13 22:30:15 -0800

    Part 2 completed.

--- day14.py
+++ day14.py
@@ -1,4 +1,5 @@
 import re
+from typing import List, Dict
 
 
 class ProgramConverter(object):
@@ -27,8 +28,42 @@ class ProgramConverter(object):
             self.update_memory(int(groups[1]), int(groups[2]))
 
 
+class ProgramConverterV2(object):
+    mem: Dict[int, int]
+    x_masks: List[str]
+
+    def __init__(self):
+        self.mem = {}
+        self.mask = ''
+        self.one_mask = ''
+        self.x_masks = []
+
+    def process_mask(self, mask: str):
+        self.mask = mask
+        self.one_mask = mask.replace('X', '0')  # Use with OR
+        self.x_masks = []
+        for i, c in enumerate(mask):
+            if c == 'X':
+                self.x_masks.append(('0'*i) + '1' + ('0' * (len(mask) - i - 1)))
+
+    def update_memory(self, loc: int, value: int):
+        addresses: List[int] = [loc | int(self.one_mask, 2)]
+        for mask in self.x_masks:
+            addresses += [a ^ int(mask, 2) for a in addresses]
+        for a in addresses:
+            self.mem[a] = value
+
+    def parse_input_line(self, line: str):
+        match = re.match(r'(mem|mask)\[?([0-9]*?)]? = ([X0-9].*)', line)
+        groups = match.groups()
+        if groups[0] == 'mask':
+            self.process_mask(groups[2])
+        else:
+            self.update_memory(int(groups[1]), int(groups[2]))
+
+
 def main():
-    program_converter = ProgramConverter()
+    program_converter = ProgramConverterV2()
     with open('inputs/14-input.txt') as input_file:
         for line in input_file:
             program_converter.parse_input_line(line)
