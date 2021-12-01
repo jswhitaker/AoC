080ec55e56c7ce9732fec0543c2f3a071be943d1
Jake Whitaker <jake.whitaker@gmail.com>
2021-11-30 21:10:44 -0800

    Day 1 completed.
    Probably going to clean this up.

--- 2021/01.py
+++ 2021/01.py
@@ -1,11 +1,32 @@
 def main():
-  with open('2020/inputs/04-input.txt') as input_file:
-  # data = input_file.read()
-  #        groups = data.split('\n\n')
-  #     for line in input_file:
-  pass
+  depths = []
+  with open('inputs/01.txt') as input_file:
+    for line in input_file:
+      depths.append(int(line))
+
+  deeper = 0
+
+  for i in range(len(depths)-1):
+    if depths[i+1] > depths[i]:
+      deeper = deeper+1
+
+  return deeper
+
+def main_p2():
+  depths = []
+  with open('inputs/01.txt') as input_file:
+    for line in input_file:
+      depths.append(int(line))
+
+  deeper = 0
+
+  for i in range(len(depths)-3):
+    if (depths[i+1] + depths[i+2] + depths[i+3]) > (depths[i] + depths[i+1] + depths[i+2]):
+      deeper = deeper+1
+
+  return deeper
 
 
 if __name__ == '__main__':
-  result = main()
+  result = main_p2()
   print(result)
