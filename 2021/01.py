c5fb7e1df64721340712a60f7a7a9e522765bac5
Jake Whitaker <jake.whitaker@gmail.com>
2021-11-30 21:28:18 -0800

    A bit cleaned up.
    Also realized the window for part 2 doesn't need the overlapping values to be compared.

--- 2021/01.py
+++ 2021/01.py
@@ -1,32 +1,22 @@
+from typing import List
+
+
 def main():
-  depths = []
-  with open('inputs/01.txt') as input_file:
-    for line in input_file:
-      depths.append(int(line))
+    depths = []
+    with open('inputs/01.txt') as input_file:
+        for line in input_file:
+            depths.append(int(line))
+    return depth_increases(depths, 1), depth_increases(depths, 3)
 
-  deeper = 0
 
-  for i in range(len(depths)-1):
-    if depths[i+1] > depths[i]:
-      deeper = deeper+1
-
-  return deeper
-
-def main_p2():
-  depths = []
-  with open('inputs/01.txt') as input_file:
-    for line in input_file:
-      depths.append(int(line))
-
-  deeper = 0
-
-  for i in range(len(depths)-3):
-    if (depths[i+1] + depths[i+2] + depths[i+3]) > (depths[i] + depths[i+1] + depths[i+2]):
-      deeper = deeper+1
-
-  return deeper
+def depth_increases(depths: List[int], length: int):
+    deeper = 0
+    for i in range(len(depths) - length):
+        if depths[i + length] > depths[i]:
+            deeper = deeper + 1
+    return deeper
 
 
 if __name__ == '__main__':
-  result = main_p2()
-  print(result)
+    result = main()
+    print(result)
