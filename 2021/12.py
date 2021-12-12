8fc8535848a97a797a25fdeb6f11bc7062192456
Jake Whitaker <jake.whitaker@gmail.com>
2021-12-11 22:41:51 -0800

    Day 12 part 2.

--- 2021/12.py
+++ 2021/12.py
@@ -28,7 +28,8 @@ class Graph(object):
         return hash(frozenset(self.nodes.items()))
 
     @cache
-    def find_path(self, start: str, end: str, visited: frozenset[str]) -> Optional[List[List[str]]]:
+    def find_path(self, start: str, end: str, visited: frozenset[str] = frozenset(),
+                  allow_small_twice: bool = False) -> List[List[str]]:
         paths = []
         if start == end:
             paths.append([end])
@@ -36,15 +37,23 @@ class Graph(object):
         if len(next_nodes) > 0:
             for n in next_nodes:
                 new_visited = set(visited)
+                next_paths = []
                 if start.islower():
                     new_visited.add(start)
-                next_paths = self.find_path(n, end, frozenset(new_visited))
+                next_paths.extend(self.find_path(n, end, frozenset(new_visited), allow_small_twice))
+                if start.islower() and (start not in ['start', 'end']) and allow_small_twice:
+                    next_paths.extend(self.find_path(n, end, visited, False))
                 if len(next_paths) > 0:
                     paths.extend([[start] + p for p in next_paths])
-        return paths
+        uniq_paths = []
+        [uniq_paths.append(p) for p in paths if p not in uniq_paths]
+        return uniq_paths
 
     def task_1(self):
         return self.find_path('start', 'end', frozenset())
+
+    def task_2(self):
+        return self.find_path('start', 'end', allow_small_twice=True)
 
 
 def main():
@@ -65,9 +74,11 @@ def main():
                 node_2 = Node(node_2_name, set(), node_2_name.isupper())
                 graph.nodes[node_2_name] = node_2
             node_2.connected.add(node_1_name)
-    return graph.task_1()
+    return graph.task_2()
 
 
 if __name__ == '__main__':
     result = main()
+    for x in result:
+        print(x)
     print('Task 1: ' + str(len(result)))
